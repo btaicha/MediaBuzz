363ec65f1611f9660386ee2b986ac1e1342bc7ce
Aicha B <aicha.bard@gmail.com>
2017-12-12 10:00:20 +0100

    commit_3

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import business.Tag;
 
 public class App {
  //test avec noubelle branche
+//nouvelle modification 2
     public static void main(String[] args) {
 
         Tag anniv = new Tag("anniversaire");
